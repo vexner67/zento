311fb65f54f8f8c1bbe5e6b711fa93898beb681c
vexner <vexner67@gmail.com>
2026-09-05 23:17:40 +0300

    feat(order): add gRPC server

--- order/cmd/order/main.go
+++ order/cmd/order/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 
 	"github.com/vexner67/zento/order/internal/config"
 	"github.com/vexner67/zento/order/internal/logger"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func main() {
@@ -18,5 +23,24 @@ func main() {
 		log.Fatal(err)
 	}
 
-	logger.Info("starting order service")
+	addr := fmt.Sprintf(":%d", cfg.GRPCPort)
+
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		logger.Error("failed to listen", "error", err)
+		return
+	}
+
+	grpcServer := grpc.NewServer()
+
+	healthServer := health.NewServer()
+	grpc_health_v1.RegisterHealthServer(grpcServer, healthServer)
+
+	healthServer.SetServingStatus("", grpc_health_v1.HealthCheckResponse_SERVING)
+
+	logger.Info("starting gRPC server", "address", addr)
+
+	if err = grpcServer.Serve(listener); err != nil {
+		logger.Error("gRPC server stopped", "error", err)
+	}
 }
--- order/go.mod
+++ order/go.mod
@@ -5,4 +5,10 @@ go 1.26.4
 require (
 	github.com/caarlos0/env/v11 v11.4.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/net v0.58.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
+	golang.org/x/text v0.41.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260526163538-3dc84a4a5aaa // indirect
+	google.golang.org/grpc v1.83.2 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
 )
